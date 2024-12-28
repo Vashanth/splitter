bf37b189cbf0dd7b452b00e4d74aa467a71851d0
Vashanth <2000.vashanth@gmail.com>
2024-12-28 20:38:42 +0530

    FRONTEND_URL in dotenv

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
